6bff2c6c0cb0673a518f8eafdf3266996e576509
sidharth <sidharthsish04931@gmail.com>
2024-03-30 13:45:15 +0530

    Added about page and home1 page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 # this will go to the books controller and take the home method
   root 'books#home1'
 
+  get 'about', to:'pages#about'
+
+  get 'home', to:'pages#home'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   
